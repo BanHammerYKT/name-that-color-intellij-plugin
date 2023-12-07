119e8092aee12be8fc5eb6bb163191d883a6d722
BanHammerYKT <bogdan2001@gmail.com>
2023-12-07 14:16:52 +0900

    Fix elements detection

--- src/main/kotlin/il/co/galex/namethatcolor/plugin/annotator/ColorAnnotator.kt
+++ src/main/kotlin/il/co/galex/namethatcolor/plugin/annotator/ColorAnnotator.kt
@@ -6,21 +6,33 @@ import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.xml.XmlTextImpl
 import com.intellij.psi.util.elementType
+import com.intellij.psi.util.findParentOfType
+import com.intellij.psi.util.parentOfType
+import com.intellij.psi.xml.XmlFile
 import il.co.galex.namethatcolor.core.manager.ColorNameFinder
 import il.co.galex.namethatcolor.core.model.HexColor
 import il.co.galex.namethatcolor.plugin.intention.NameColorIntention
 import il.co.galex.namethatcolor.plugin.util.*
-import org.jetbrains.kotlin.lexer.KtTokens.IDENTIFIER
-import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes.INTEGER_CONSTANT
+import org.jetbrains.kotlin.lexer.KtTokens.*
+import org.jetbrains.kotlin.psi.KtConstantExpression
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtLiteralStringTemplateEntry
+import org.jetbrains.kotlin.psi.psiUtil.getPrevSiblingIgnoringWhitespaceAndComments
 
 class ColorAnnotator : Annotator {
 
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
 
         try {
-            if (element.containingFile.fileType.name.lowercase() == "kotlin" &&
-                element.elementType in setOf(IDENTIFIER, INTEGER_CONSTANT)) {
+            element.findParentOfType<KtFile>()?.let {
                 val color = HexColor(element.text)
+                val prev = element.getPrevSiblingIgnoringWhitespaceAndComments(false)
+                if (prev?.text in setOf("fun", "val")) return
+                if (element.elementType == REGULAR_STRING_PART || element is KtLiteralStringTemplateEntry) return
+                if (element.elementType == INTEGER_LITERAL) return
+                if (prev?.elementType == EQ && element is KtConstantExpression) return
+//                Log.info("prev ${prev.toString()} ${prev?.text} ${prev?.javaClass?.name}")
+//                Log.info("elem ${element.toString()} ${element.javaClass.name} ${color.input}")
 
                 // color list from name that color
                 holder.newAnnotation(HighlightSeverity.ERROR, COLOR_ANNOTATION_MESSAGE_KT)
@@ -38,21 +50,23 @@ class ColorAnnotator : Annotator {
 
 
         try {
-            // we found a value that is a valid color, checking that it is not already in a <color> tag
-            if ((element.parent as XmlTextImpl).parentTag?.name == RESOURCES_TAG_NAME) {
-                val color = HexColor(element.text)
+            element.parentOfType<XmlFile>()?.let {
+                // we found a value that is a valid color, checking that it is not already in a <color> tag
+                if ((element.parent as XmlTextImpl).parentTag?.name == RESOURCES_TAG_NAME) {
+                    val color = HexColor(element.text)
 
-                // color list from name that color
-                holder.newAnnotation(HighlightSeverity.ERROR, COLOR_ANNOTATION_MESSAGE)
-                    .newFix(NameColorIntention(NAME_THAT_COLOR, color, ColorNameFinder::findColor))
-                    .registerFix()
-                    .create()
+                    // color list from name that color
+                    holder.newAnnotation(HighlightSeverity.ERROR, COLOR_ANNOTATION_MESSAGE)
+                            .newFix(NameColorIntention(NAME_THAT_COLOR, color, ColorNameFinder::findColor))
+                            .registerFix()
+                            .create()
 
-                // material color palette
-                holder.newAnnotation(HighlightSeverity.ERROR, MATERIAL_COLOR_ANNOTATION_MESSAGE)
-                    .newFix(NameColorIntention(NAME_THAT_MATERIAL_COLOR, color, ColorNameFinder::findMaterialColor))
-                    .registerFix()
-                    .create()
+                    // material color palette
+                    holder.newAnnotation(HighlightSeverity.ERROR, MATERIAL_COLOR_ANNOTATION_MESSAGE)
+                            .newFix(NameColorIntention(NAME_THAT_MATERIAL_COLOR, color, ColorNameFinder::findMaterialColor))
+                            .registerFix()
+                            .create()
+                }
             }
         } catch (e: Exception) {
             // found that a color is not valid for us, ignoring it
